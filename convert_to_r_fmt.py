88d9c655bf879c3c9d888c4ce0bfb3fd5b01f873
Rob Renaud <rrenaud@gmail.com>
2011-08-31 22:23:04 -0400

    added support for different formats

--- convert_to_r_fmt.py
+++ convert_to_r_fmt.py
@@ -10,22 +10,16 @@ import game
 import random
 import utils
 
-def declare_multi_feature_extractor(feature_names):
-    def dec(func):
-        def wrapped(*args):
-            ret = func(*args)
-            assert len(ret) == len(wrapped.feature_names)
-            return ret
-        wrapped.feature_names = [f.replace(' ', '_') for f in feature_names]
-        return wrapped
-    return dec
+def nice_feature_name(n):
+    return n.replace(' ', '_').replace("'", '')
 
-@declare_multi_feature_extractor(ci.card_names())
 def composition_deck_extractor(deck_comp, game_state, player):
     ret = []
     for card in ci.card_names():
         ret.append(deck_comp.get(card, 0))
     return ret
+composition_deck_extractor.feature_names = map(nice_feature_name, 
+                                               ci.card_names())
 
 def score_deck_extractor(deck_comp, game_state, player):
     return [game_state.player_score(player)]
@@ -42,9 +36,10 @@ def action_balance_deck_extractor(deck_comp, game_state, player):
 def unique_deck_extractor(deck_comp, game_state, player):
     return [len(deck_comp)]
 
-def outcome_common_extractor(g, game_state):
+def outcome_special_extractor(g, game_state):
     turn_order = game_state.player_turn_order()
-    return [g.get_player_deck(turn_order[0]).WinPoints()]
+    win_points = g.get_player_deck(turn_order[0]).WinPoints()
+    return [win_points]
 
 def turn_tiebreaker_common_extractor(g, game_state):
     current_turn_order = game_state.player_turn_order()
@@ -73,20 +68,21 @@ def prov_or_colony_low_extractor(g, game_state):
 def turn_no_common_extractor(g, game_state):
     return [game_state.turn_index()]
 
-@declare_multi_feature_extractor(ci.card_names())
 def supply_common_extractor(g, game_state):
     ret = []
     for card in ci.card_names():
         ret.append(game_state.supply.get(card, 0))
     return ret
+supply_common_extractor.feature_names = map(nice_feature_name, ci.card_names())
 
 def make_extractor_list(suffix):
     extractor_names = [n[:-len(suffix)] for n in 
                        globals() if n.endswith(suffix)]
     extractors = [eval(n + suffix) for n in extractor_names]
+
     for extractor, name in itertools.izip(extractors, extractor_names):
         if not hasattr(extractor, 'feature_names'):
-            extractor.feature_names = [name] 
+            extractor.feature_names = [name]
     return extractors
 
 _deck_extractor_list = make_extractor_list('deck_extractor')
@@ -98,12 +94,8 @@ def feature_names(feature_extractor_list):
         ret.extend(extractor.feature_names)
     return ret
 
-def encode_state_r_fmt(g, game_state, encode_id=False):
+def state_to_features(g, game_state):
     output_list = []
-    if encode_id:
-        assert False
-        output_list.append(g.get_id() + '#' + 
-                           game_state.turn_label(for_anchor=True))
 
     for common_extractor in _common_extractor_list:
         output_list.extend(common_extractor(g, game_state))
@@ -122,51 +114,88 @@ def encode_state_r_fmt(g, game_state, encode_id=False):
     for p1_feature_val, p2_feature_val in itertools.izip(p1, p2):
         output_list.append(p1_feature_val - p2_feature_val)
     
+    output_list.extend(outcome_special_extractor(g, game_state))
     return output_list
 
-def output_state(state, output_file):
-    formatted_str = u' '.join(map(unicode, state))
-    actual = formatted_str.count(' ') 
-    expected = len(make_header()) - 1
-    assert actual == expected, '%s %d %d' % (state[0], actual, expected)
+def output_state(state, output_file, sep=' '):
+    formatted_str = sep.join(map(unicode, state))
     output_file.write(formatted_str)
     output_file.write('\n')
 
-def make_header():
+def get_all_feature_names():
     header = feature_names(_common_extractor_list)
     
     for player_label in ['my', 'opp', 'diff']:
         for feature_name in feature_names(_deck_extractor_list):
             header.append(player_label + '_' + feature_name)
-                           
+       
+    header.append('outcome_')
     return header
 
-def write_header(output_file):
-    outputted = ' '.join(make_header()) + '\n'
+def write_r_header(output_file):
+    outputted = ' '.join(get_all_feature_names()) + '\n'
     output_file.write(outputted)
+
+def write_weka_header(output_file, force_classification):
+    output_file.write('@RELATION isotropic_games\n\n')
+    for feature_name in get_all_feature_names()[:-1]:
+        output_file.write('@ATTRIBUTE %s NUMERIC\n' % feature_name)
+    assert force_classification
+    output_file.write('@ATTRIBUTE outcome_ {0,1}')
+    output_file.write('\n@DATA\n')
+
+def output_libsvm_state(state, output_file):
+    if state[-1] == 0:
+        output_file.write('-1 ')
+    else:
+        output_file.write('1 ')
+    for index, value in enumerate(state[:-1]):
+        if value != 0:
+            output_file.write('%d:%d ' % (index + 1, value))
+    output_file.write('\n')
 
 def main():
     c = utils.get_mongo_connection()
 
-    output_file = open('r_format.data', 'w')
-    write_header(output_file)
+    force_classification = True
+
+    prefix = 'data/test_huge_'
+    # limit = 10000
+    r_output_file = open(prefix + 'r_format.data', 'w')
+    weka_output_file = open(prefix + 'games.arff', 'w')
+    librf_output_file = open(prefix + 'librf_games.csv', 'w')
+    librf_labels_file = open(prefix + 'librf_games_labels.txt', 'w')
+    libsvm_output_file = open(prefix + 'libsvm_games.txt', 'w')
+    write_r_header(r_output_file)
+    write_weka_header(weka_output_file, force_classification)
     
     for raw_game in utils.progress_meter(
         c.test.games.find(
-            #{'_id': {'$gt': 'game-20110230'} }
-            ), 1000):
+            {'_id': {'$gt': 'game-20110715'} }
+            ), 100):
         g = game.Game(raw_game)
         if g.dubious_quality() or len(g.get_player_decks()) != 2:
             continue
+        if force_classification and g.get_player_decks()[0].WinPoints() == 1.0:
+            print 'skipping tie'
 
-        encoded_states = []
-        for game_state in g.game_state_iterator():
-            encoded_state_list = encode_state_r_fmt(g, game_state)
-            encoded_states.append(encoded_state_list)
+        saved_turn_ind = random.randint(0, len(g.get_turns()) - 1)
+        for ind, game_state in enumerate(g.game_state_iterator()):
+            # if ind == saved_turn_ind:
+                encoded_state = state_to_features(g, game_state)
+                if force_classification:
+                    encoded_state[-1] = int(encoded_state[-1] / 2)
+                #output_state(encoded_state, r_output_file, ' ')
+                #output_state(encoded_state, weka_output_file, ',')
 
-        # only pick one state from each game to avoid overfitting.
-        picked = random.choice(encoded_states)
-        output_state(picked, output_file)
+                #output_state(encoded_state[:-1], librf_output_file, ',')
+                #librf_labels_file.write('%d\n' % encoded_state[-1])
+
+                output_libsvm_state(encoded_state, libsvm_output_file)
+        #else:
+        #    assert False, ('did not find turn %d in %s' % (saved_turn_ind,
+                                                           # game.get_id()))
+
                 
 if __name__ == '__main__':
     main()
